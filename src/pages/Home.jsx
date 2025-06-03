ba794147c12af7310aa5198f05235b9cec6589cf
mohammad <ekhlasimohammad345@gmail.com>
2025-06-03 21:38:11 +0200

    home page css styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-
+import './Home.css'
 
 
 function Home (){
     
     return(
         <>
-
-        <h2>Home</h2>
-        <h3>Welcome TO the cooking class. please select a recepi under the categories menu</h3>
+        
+        <div className="homeText">
+        <h1 className="homeTitle">Cooking Class</h1>
+        <p className="homeDescription">
+            Welcome to the cooking class, where we explore delicious recipes from around the world. </p>
+        <p> please select a recipe under the categories menu</p>
+        </div>
+        <div className="imgContainer">
+        <img src="https://img.freepik.com/free-photo/top-view-food-ingredients-with-vegetable-soup-bowl-notebook_23-2148834720.jpg?semt=ais_hybrid&w=740" alt="homeImage" className="homeImage"/>
+        </div>
 
         </>  
     )
