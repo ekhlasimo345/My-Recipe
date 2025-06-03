d25ffd14e440474114fe4815f3b2b8f0ed44369a
mohammad <ekhlasimohammad345@gmail.com>
2025-06-03 23:30:16 +0200

    css changes on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,17 @@ function Home (){
     
     return(
         <>
-        
-        <div className="homeText">
-        <h1 className="homeTitle">Cooking Class</h1>
-        <p className="homeDescription">
-            Welcome to the cooking class, where we explore delicious recipes from around the world. </p>
-        <p> please select a recipe under the categories menu</p>
+        <div className="homeBackground">
+            <div className="homeText">
+            <h1 className="homeTitle">Cooking Class</h1>
+            <p className="homeDescription">
+                Welcome to the cooking class, where we explore delicious recipes from around the world. </p>
+            <p> please select a recipe under the categories menu</p>
+            </div>
+            <div className="imgContainer">
+            <img src="https://img.freepik.com/free-photo/top-view-food-ingredients-with-vegetable-soup-bowl-notebook_23-2148834720.jpg?semt=ais_hybrid&w=740" alt="homeImage" className="homeImage"/>
+            </div>
         </div>
-        <div className="imgContainer">
-        <img src="https://img.freepik.com/free-photo/top-view-food-ingredients-with-vegetable-soup-bowl-notebook_23-2148834720.jpg?semt=ais_hybrid&w=740" alt="homeImage" className="homeImage"/>
-        </div>
-
         </>  
     )
 }
